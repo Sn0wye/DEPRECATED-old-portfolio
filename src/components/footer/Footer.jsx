9dc80044475053c3e0ca4c23c9b63eedd6ee1b3f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-04-05 19:41:18 -0300

    finished the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
+import { FaFacebookF } from 'react-icons/fa';
+import { FiInstagram } from 'react-icons/fi';
+import { IoLogoTwitter } from 'react-icons/io';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        LOGO
+      </a>
 
-export default Footer
+      <ul className="permalinks">
+        <a href="#">Home</a>
+        <a href="#about">About</a>
+        <a href="#experience">Experience</a>
+        <a href="#services">Services</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#testimonials">Testimonials</a>
+        <a href="#contact">Contact</a>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FiInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <IoLogoTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Sn0wye/Gabriel Trzimajewski. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
