34a2bba0b6d5d42071019b3d220d63eb11a35ba7
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-09 21:59:27 -0300

    Update experience component with supabase fetch

--- src/Components/experience/Experience.tsx
+++ src/Components/experience/Experience.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import "./experience.scss";
 import { BsPatchCheckFill } from "react-icons/bs";
+import { supabase } from "../../services/supabase-config";
+import { useState } from "react";
+
+type ExperienceData = {
+  id: string;
+  name: string;
+  description: string;
+}[];
 
 const Experience = () => {
+  const [frontendData, setFrontendData] = useState<ExperienceData | null>(null);
+  const [backendData, setBackendData] = useState<ExperienceData | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const { data, error } = await supabase.from("Frontend").select("*");
+      if (error) throw error;
+      setFrontendData(data);
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const { data, error } = await supabase.from("Backend").select("*");
+      if (error) throw error;
+      setBackendData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useLayoutEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section id="experience">
       <h5>The Skills I Have</h5>
@@ -12,106 +44,32 @@ const Experience = () => {
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>JavaScript</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>React</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Framer Motion</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Sass and CSS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Axios</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Tailwind</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+            {frontendData &&
+              frontendData.map((item) => (
+                <article key={item.id} className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{item.name}</h4>
+                    <small className="text-light">{item.description}</small>
+                  </div>
+                </article>
+              ))}
           </div>
         </div>
         {/*---------- END OF FRONTEND {/*----------*/}
         <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Node.JS</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Express</h4>
-                <small className="text-light">Beginner</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Firebase</h4>
-                <small className="text-light">Intermediate</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>MongoDB</h4>
-                <small className="text-light">Beginner</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>SQLite</h4>
-                <small className="text-light">Beginner</small>
-              </div>
-            </article>
-
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Prisma</h4>
-                <small className="text-light">Beginner</small>
-              </div>
-            </article>
+            {backendData &&
+              backendData.map((item) => (
+                <article key={item.id} className="experience__details">
+                  <BsPatchCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{item.name}</h4>
+                    <small className="text-light">{item.description}</small>
+                  </div>
+                </article>
+              ))}
           </div>
         </div>
       </div>
