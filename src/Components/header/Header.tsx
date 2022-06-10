32197730fd73b7b1f3faa959e30a426c5a01e8f0
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-06-09 21:24:32 -0300

    Refresh image alt

--- src/Components/header/Header.tsx
+++ src/Components/header/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
         <HeaderSocial />
 
         <div className="me">
-          <img src={ME} alt="me" />
+          <img src={ME} alt="Gabriel Trzimajewski Photo" />
         </div>
 
         <a href="#contact" className="scroll__down">
