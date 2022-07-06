0562f763efc6b649148d78bce5070a6329ce3bb8
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-07-06 16:30:10 -0300

    fix styling on link

--- src/Components/styled/Button.ts
+++ src/Components/styled/Button.ts
@@ -25,24 +25,33 @@ const Button = styled("button", {
 });
 
 const Link = styled("a", {
-  width: "max-content",
-  display: "inline-block",
   color: "$primary",
-  padding: "0.75rem 1.2rem",
-  borderRadius: "0.4rem",
-  cursor: "pointer",
-  border: "1px solid $primary",
   transition: "$transition",
   "&:hover": {
-    background: "$white",
-    color: "$bg",
-    borderColor: "transparent",
+    color: "$white",
   },
   variants: {
     color: {
       primary: {
         background: "$primary",
         color: "$bg",
+      },
+    },
+    type: {
+      button: {
+        width: "max-content",
+        display: "inline-block",
+        color: "$bg",
+        padding: "0.75rem 1.2rem",
+        borderRadius: "0.4rem",
+        cursor: "pointer",
+        border: "1px solid $primary",
+        transition: "$transition",
+        "&:hover": {
+          background: "$white",
+          color: "$bg",
+          borderColor: "transparent",
+        },
       },
     },
   },
